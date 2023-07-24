914b4804cc685fd502e304b1a60bfd188b3717f0
qrio-ishidatakuya <takuya.ishida@qrioinc.com>
2023-07-24 16:25:44 +0900

    dotenv入れる

--- src/request.ts
+++ src/request.ts
@@ -1,13 +1,28 @@
 import { Configuration, EsaApi } from "../client";
 import type { AxiosRequestConfig } from 'axios';
 
+// import fs from 'fs';
+// const outputFilepath:string = './data/output/getpost';
+
+// const outputFile = async (data: any) => {
+//   try {
+//     fs.writeFile(outputFilepath,data, (err) =>
+//     {
+//       console.log(err);
+//     });
+//   } catch (e) {
+//     console.log(e);
+//   }
+// };
+
 (async () => {
   const esaApi = new EsaApi(
     new Configuration({ accessToken: process.env.ESA_API_TOKEN })
   );
-  const { data } = await esaApi.getPosts({ teamName: "doc" });
+  const { data } = await esaApi.getPosts({ teamName: "qrio" });
 
   for (const post of data.posts) {
+    // outputFile(post.name);
     console.log(post.name);
   }
 })();
